c9a71fea71d60edac4af253784ed2f5f04e8c067
Tanya Samastroyenka <tanyasamastr@gmail.com>
2021-10-19 11:28:25 +0300

    Initial Commit

---
+++ MemoryG/MemoryGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGApp.swift
+//  MemoryG
+//
+//  Created by Tanya Samastroyenka on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
